5831a9a96b49052bd294f1a4d998c54d66c91c07
eman-a-dev <emanarif149@gmail>
2024-04-05 22:36:26 +0500

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="font-bold text-[#128C7E] text-4xl">WhatsApp clone coming in 7 days!</h1>
+    </div>
+  );
+}
